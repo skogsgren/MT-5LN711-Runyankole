dbf51c3f774355c66c1ad622278af5dfd2919931
y-he2 <he.yiyang@outlook.com>
2024-10-27 15:34:33 +0100

    update test training scripts

--- src/test_model/train_test_model.sh
+++ src/test_model/train_test_model.sh
@@ -2,5 +2,5 @@
 # AUTHOR=Gustaf Gren
 # NOTES=Based on the OpenNMT tutorial:
 # https://github.com/ymoslem/OpenNMT-Tutorial/blob/main/2-NMT-Training.ipynb
-onmt_build_vocab -config config.yaml -n_sample -1 -num_threads 2
-onmt_train -config config.yaml
+# onmt_build_vocab -config config.yaml -n_sample -1 -num_threads 12
+onmt_train -config config.yaml -num_threads 12
