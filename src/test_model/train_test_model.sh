0fadd633b3528877c78fdfe1d557d18e79174dd1
y-he2 <he.yiyang@outlook.com>
2024-10-29 16:53:45 +0100

    added roughly auto training

--- src/test_model/train_test_model.sh
+++ src/test_model/train_test_model.sh
@@ -2,5 +2,5 @@
 # AUTHOR=Gustaf Gren
 # NOTES=Based on the OpenNMT tutorial:
 # https://github.com/ymoslem/OpenNMT-Tutorial/blob/main/2-NMT-Training.ipynb
-# onmt_build_vocab -config ../current_config.yaml -n_sample -1 -num_threads 12
-onmt_train -config ../current_config.yaml -num_threads 12
+# onmt_build_vocab -config current_config.yaml -n_sample -1 -num_threads 12
+onmt_train -config current_config.yaml -num_threads 12
